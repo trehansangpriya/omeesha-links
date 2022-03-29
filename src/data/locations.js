d42f1aeb2b15f214a1f80a0cfc191155162aee92
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-03-29 21:12:09 +0530

    update url

--- src/data/locations.js
+++ src/data/locations.js
@@ -1,7 +1,7 @@
 const locations = [
     {
         name: "Omeehsa Salon - Shalimar Market",
-        url: "https://g.page/omeesha?share",
+        url: "https://www.google.com/maps/place/Omeesha+Makeovers/@26.7944957,82.7313102,17z/data=!3m1!4b1!4m5!3m4!1s0x3990c554c0450859:0xc9f9c62ad9c34082!8m2!3d26.7944948!4d82.7334973",
         embed: <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1205.7720861431026!2d82.73205391604111!3d26.794391046076587!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc9f9c62ad9c34082!2sOmeesha%20Makeovers!5e0!3m2!1sen!2sin!4v1648565357034!5m2!1sen!2sin" width="100%" height="300" style={{ border: 0, borderRadius: '6px' }} allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     },
     {
