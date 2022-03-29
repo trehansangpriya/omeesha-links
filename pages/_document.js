0672a21f83ef8aa138b0ec0176c89fa1efa846ba
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-03-29 20:27:14 +0530

    branding

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,8 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+                    {/* icon */}
+                    <link rel="icon" href="/assets/icons/logo.png" />
                 </Head>
                 <body className='bg-layout-800 text-layout-100 flex justify-center w-full'>
                     <Main />
