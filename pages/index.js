5dffe42ba549e055b9a6de4593ccd0ed089481d7
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-05-05 20:06:30 +0530

    loading

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Logo from '@/Components/Logo'
-import links from "@/Data/links"
 import Link from '@/Components/Link'
-import locations from '@/Data/locations'
 import Location from '@/Components/Location'
 import { collection, onSnapshot } from 'firebase/firestore'
 import { db } from 'src/firebase'
@@ -29,17 +28,26 @@ const Home = () => {
       <div className='flex flex-col items-center w-full min-w-[340px] min-h-screen p-4 py-[56px] md:w-[400px]'>
         <Logo />
         {
-          links.map((link, index) => (
-            <Link {...link} key={index} />
-          ))
+          !loading ? (
+            <>
+              {
+                links.map((link, index) => (
+                  <Link {...link} key={index} />
+                ))
+              }
+              <br />
+              <p className='w-full text-headingm my-1 font-heading'>Addresses</p>
+              {
+                locations.map((location, index) => (
+                  <Location {...location} key={index} />
+                ))
+              }
+            </>
+          ) : <div className="flex h-[200px] justify-center items-center">
+            <img src="/assets/gifs/loading.gif" alt="Loading" width='72px' />
+          </div>
         }
-        <br />
-        <p className='w-full text-headingm my-1 font-heading'>Addresses</p>
-        {
-          locations.map((location, index) => (
-            <Location {...location} key={index} />
-          ))
-        }
+
       </div>
     </>
   )
